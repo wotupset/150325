d22217686b91f8a45648cc68ceb61210909e7ef4
wotupset <wotupset@gmail.com>
2015-04-03 12:06:21 +0900

    aaaaaa

--- 01.js
+++ 01.js
@@ -1,29 +1,12 @@
-function text_on(id,x){
-	var FFF='';
-	FFF=document.getElementById("body").innerHTML;
-	document.getElementById("body").innerHTML='<span id="'+id+'">'+x+'</span>'+FFF;
+function poi(){
+	$('div#ddd').html('');
 	//
-	$("#body").click(function(){
-		t++;
-	});
-	//
-	var t=0;
-	var cc=0;
-	while(!t){
-		cc++;
-		document.getElementById(id).innerHTML=cc;
-		timedown_y(id);
-	}
-	//
-	//$("#"+id+"").remove();//Ãö³¬¤¸¯À
-}
-
-function timedown_y(id){
 	var timedown_x = setInterval(function() {
-		if(t==0){
+		if(t<3600){
+			$('div#ddd').html(t);
 			timedown_x;//Ä~Äò¶]°j°é
 		}else{
 			clearInterval(timedown_x);//Ãö³¬°j°é
 		}
-	}, 100);
+	}, 1000);
 }
